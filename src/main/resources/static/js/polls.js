80dd2f2de00de9489f96653495e9b5b21a904adc
Jon Inloes <rr2real@gmail.com>
2016-01-22 15:42:27 -0800

    added create poll page

--- src/main/resources/static/js/polls.js
+++ src/main/resources/static/js/polls.js
@@ -5,13 +5,21 @@ var app = angular.module('polls', ['ngRoute'])
                     templateUrl: 'home.html',
                     controller: 'home'
                 })
+                .when('/create_poll', {
+                    templateUrl: 'create_poll.html'
+                })
                 .otherwise('/');
         })
-        .controller('navigation', function ($rootScope, $scope, $http, $location, $route) {
+        .controller('navigation', function($rootScope, $scope, $http, $location, $route) {
             $scope.tab = function (route) {
                 return $route.current && route === $route.current.controller;
             };
         })
-        .controller('home', function ($scope, $http) {
+        .controller('home', function($scope, $http) {
+        })
+        .controller('CreatePollController', function($scope, $window, $http) {
+            $scope.create = function(poll) {
+                $window.alert(poll.name);
+            }
         })
     ;
